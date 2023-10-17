dbd0a9a52b5caae9d90849ea8ea54ff5935e6bfe
DaOfficialWizard <45744329+ZanzyTHEbar@users.noreply.github.com>
2023-10-17 22:57:45 +0100

    feat: os detection

--- src/app.tsx
+++ src/app.tsx
@@ -1,3 +1,4 @@
+import { platform } from '@tauri-apps/api/os'
 import { lazy, onMount, Suspense } from 'solid-js'
 import { useAppContextMain } from './store/context/main'
 import { AppProvider } from '@store/context/app'
@@ -7,8 +8,15 @@ const AppRoutes = lazy(() => import('@routes/Routes'))
 const App = () => {
     const { handleAppBoot } = useAppContextMain()
     //const ref = document.getElementById('titlebar')
+    const setupOs = async () => {
+        const platformName = await platform()
+        if (platformName) {
+            document.body.classList.add(platformName)
+        }
+    }
     onMount(() => {
         handleAppBoot()
+        setupOs()
     })
 
     return (
